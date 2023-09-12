3326f2cc4c5229478e996d1924e87a9470cc2e3d
jiabin.chen <jiabin.chen@amlogic.com>
2023-09-12 14:21:32 +0800

    wifi: fix build error [1/1]
    PD#SWPL-137988
    
    Problem:
    build error
    
    Solution:
    fix build error
    
    Verify:
    ohm
    
    Change-Id: I378b07bd463e9d973f4f422d7f848d5136507e06
    Signed-off-by: jiabin.chen <jiabin.chen@amlogic.com>

--- aml_drv/aml_scc.c
+++ aml_drv/aml_scc.c
@@ -20,6 +20,10 @@
 #include "aml_wq.h"
 #include "aml_sap.h"
 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(5, 15, 41) && defined (CONFIG_AMLOGIC_KERNEL_VERSION))
+#include <linux/upstream_version.h>
+#endif
+
 #define CSA_COUNT               10
 #define CSA_BLOCK_TX            1
 
@@ -300,7 +304,11 @@ void aml_scc_csa_finish(struct work_struct *ws)
             aml_txq_vif_stop(vif, AML_TXQ_STOP_CHAN, aml_hw);
         spin_unlock_bh(&aml_hw->cb_lock);
 #ifdef CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT
+        #if ((defined (AML_KERNEL_VERSION) && AML_KERNEL_VERSION >= 15) || LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0))
+        cfg80211_ch_switch_notify(vif->ndev, &csa->chandef, 0, 0);
+        #else
         cfg80211_ch_switch_notify(vif->ndev, &csa->chandef, 0);
+        #endif
 #else
         cfg80211_ch_switch_notify(vif->ndev, &csa->chandef);
 #endif
@@ -485,7 +493,11 @@ static int aml_scc_channel_switch(struct aml_hw *aml_hw, struct aml_vif *vif, st
         INIT_WORK(&csa->work, aml_scc_csa_finish);
 #ifndef CONFIG_PT_MODE
     #ifdef CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT
+        #if ((defined (AML_KERNEL_VERSION) && AML_KERNEL_VERSION >= 15) || LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0))
+        cfg80211_ch_switch_started_notify(dev, &csa->chandef, 0, CSA_COUNT, CSA_BLOCK_TX, 0);
+        #else
         cfg80211_ch_switch_started_notify(dev, &csa->chandef, 0, CSA_COUNT, CSA_BLOCK_TX);
+        #endif
     #else
         cfg80211_ch_switch_started_notify(dev, &csa->chandef, CSA_COUNT);
     #endif
@@ -569,7 +581,11 @@ void aml_scc_check_chan_conflict(struct aml_hw *aml_hw)
                                 aml_chanctx_unlink(vif);
                                 aml_chanctx_link(vif, target_vif->ch_index, &target_chdef);
                                 #ifdef CFG80211_SINGLE_NETDEV_MULTI_LINK_SUPPORT
+                                #if ((defined (AML_KERNEL_VERSION) && AML_KERNEL_VERSION >= 15) || LINUX_VERSION_CODE >= KERNEL_VERSION(6, 3, 0))
+                                cfg80211_ch_switch_notify(vif->ndev, &target_chdef, 0, 0);
+                                #else
                                 cfg80211_ch_switch_notify(vif->ndev, &target_chdef, 0);
+                                #endif
                                 #else
                                 cfg80211_ch_switch_notify(vif->ndev, &target_chdef);
                                 #endif
